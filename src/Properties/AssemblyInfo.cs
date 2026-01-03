d4e55fe0c6cb103b8b574f5809405bf78e3e95e7
Marcy <87911841+DemonBunnyBon@users.noreply.github.com>
2026-01-03 14:26:47 +0100

    2.0.3

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using Il2CppHoloville.HOTween.Core.Easing;
+using MelonLoader;
+using PineNeedleTeaMod;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("PineNeedleTea")]
+[assembly: AssemblyCopyright("Marcy")]
+
+
+[assembly: AssemblyVersion("2.0.3")]
+[assembly: AssemblyFileVersion("2.0.3")]
+[assembly: MelonInfo(typeof(Implementation), "PineNeedleTea", "2.0.3", "Marcy", null)]
+
+
+[assembly: MelonGame("Hinterland", "TheLongDark")]
